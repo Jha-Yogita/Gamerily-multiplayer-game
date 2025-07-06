56e9eca8a1916d4cc3567ab3fddf3d8155585a78
Jha-Yogita <yogitajha5@gmail.com>
2025-07-06 16:20:31 +0530

    signup9

--- backend/controllers/gameController.js
+++ backend/controllers/gameController.js
@@ -1,7 +1,8 @@
 const Question = require("../data/import.js");
 const { rooms, completedPlayers } = require("../state");
 
-// Submit results
+const finalizedResults = {};
+
 exports.submitResults = (req, res) => {
   const { roomId, username, score, totalTime } = req.body;
   if (!roomId || !username || score === undefined || totalTime === undefined) {
@@ -22,6 +23,7 @@ exports.submitResults = (req, res) => {
 exports.checkResults = (req, res) => {
   const { roomId } = req.body;
 
+  // Return cached final results if available
   if (finalizedResults[roomId]) {
     return res.json(finalizedResults[roomId]);
   }
@@ -35,11 +37,38 @@ exports.checkResults = (req, res) => {
   const p2Data = completedPlayers[roomId][p2.username];
 
   if (!p1Data || !p2Data) {
-    return res.json({ waiting: true });
+    return res.json({ waiting: true }); // One player still not submitted
   }
 
-  return res.json({ waiting: true }); // Wait until result is finalized
+  // Both players submitted, calculate winner
+  let winner;
+  if (p1Data.score > p2Data.score) {
+    winner = p1.username;
+  } else if (p2Data.score > p1Data.score) {
+    winner = p2.username;
+  } else {
+    // Tie-breaker: shorter total time wins
+    winner = p1Data.totalTime < p2Data.totalTime ? p1.username : p2.username;
+  }
+
+  const resultPayload = {
+    player1: { username: p1.username, ...p1Data },
+    player2: { username: p2.username, ...p2Data },
+    winner
+  };
+
+  finalizedResults[roomId] = resultPayload;
+
+  // Cleanup after 2 minutes
+  setTimeout(() => {
+    delete finalizedResults[roomId];
+    delete completedPlayers[roomId];
+    delete rooms[roomId];
+  }, 120000);
+
+  return res.json(resultPayload);
 };
+
 // Genres
 exports.getGenres = (req, res) => {
   res.json([
